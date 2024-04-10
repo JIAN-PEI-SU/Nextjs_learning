79b8099c638b74ad540a72951a891d89ba3101eb
JIAN-PEI-SU <108530704+JIAN-PEI-SU@users.noreply.github.com>
2024-04-10 10:00:23 +0800

    20240410-restart

--- learning/src/components/ui/index.tsx
+++
@@ -1,7 +0,0 @@
-export { Header } from "./Header"
-export { Footer } from "./Footer"
-export { SideBar } from "./SideBar"
-export { Navigation } from "./Navigation"
-export { Main } from "./Main"
-export { Body } from "./Body"
-export { Html } from "./Html"
